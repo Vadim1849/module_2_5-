e909c9d4cb7de5f7ca70c707697abb624699f603
Vadim1849 <vabutko1999@mail.ru>
2024-09-18 20:50:12 +0300

    Update module_2_5.py

--- module_2_5.py
+++ module_2_5.py
@@ -10,6 +10,10 @@ def get_matrix(n, m, value):
 result1 = get_matrix(2, 2, 10)
 result2 = get_matrix(3, 5, 42)
 result3 = get_matrix(4, 2, 13)
+result4 = get_matrix(0, 2, 10)  # Примечания: со значением 0 или меньше, будет возвращаться пустой список
+result5 = get_matrix(3, 0, 42)
 print(result1)
 print(result2)
 print(result3)
+print(result4)
+print(result5)
